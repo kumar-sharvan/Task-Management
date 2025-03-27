bc116079196d5f553c829d39d647386f7d050c18
Sharvan Kumar <162790236+kumar-sharvan@users.noreply.github.com>
2025-03-27 17:16:40 +0530

    Update TaskItem.jsx

--- Frontend/src/components/TaskItem.jsx
+++ Frontend/src/components/TaskItem.jsx
@@ -4,25 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const TaskItem = ({ task, onUpdateTask, onDeleteTask }) => {
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <div>
+      <div className="d-flex align-items-center gap-2 w-100">
         <input
           type="checkbox"
-          className="form-check-input me-2"
+          className="form-check-input ms-2"
           checked={task.completed}
           onChange={() =>
             onUpdateTask(task._id, { ...task, completed: !task.completed })
           }
         />
         <span
-          className={`task-text ${
-            task.completed ? "text-decoration-line-through" : ""
+          className={`task-text text-start flex-grow-1 ms-2 ${
+            task.completed ? "text-decoration-line-through text-muted" : ""
           }`}
         >
           <strong>{task.title}</strong> - {task.description}
         </span>
       </div>
       <button
-        className="btn btn-danger btn-sm mb-2"
+        className="btn btn-danger btn-sm"
         onClick={() => onDeleteTask(task._id)}
       >
         Delete
